a602806a7d6368a22d8ac2b1a069bb3bef94e57f
Paulsen <mail@paulsen.ooo>
2022-12-23 02:01:49 +0100

    V1.0.3 Completed HowTo-Book
    - HowTo-Book
    - Extended default-drop-table

--- src/main/java/ooo/paulsen/mc/extended_pvp/HowTo.java
+++ src/main/java/ooo/paulsen/mc/extended_pvp/HowTo.java
@@ -1,10 +1,6 @@
 package ooo.paulsen.mc.extended_pvp;
 
-import net.md_5.bungee.api.ChatColor;
-import net.md_5.bungee.api.chat.ClickEvent;
-import net.md_5.bungee.api.chat.ComponentBuilder;
-import net.md_5.bungee.api.chat.TextComponent;
-import org.bukkit.Color;
+import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -23,22 +19,7 @@ public class HowTo implements CommandExecutor {
     private static ItemStack howToBook;
 
     public HowTo() {
-        howToBook = new ItemStack(Material.WRITTEN_BOOK);
-        BookMeta bookMeta = (BookMeta) howToBook.getItemMeta();
-
-        bookMeta.setAuthor("Blank");
-        bookMeta.setTitle("Blank");
-        bookMeta.setLore(Arrays.asList("This is a HowTo-Book", "of the useful", "Extended PvP plugin!"));
-
-        TextComponent t = new TextComponent("/howto-epvp");
-        t.setColor(ChatColor.LIGHT_PURPLE);
-        t.setUnderlined(true);
-        t.setBold(true);
-
-
-        bookMeta.setPages(ChatColor.BLUE + "Dei mam stinkt!", ChatColor.DARK_BLUE + "Mmmhhh, LOL!", t.toLegacyText());
-
-        howToBook.setItemMeta(bookMeta);
+        generateBook();
     }
 
     @Override
@@ -51,7 +32,57 @@ public class HowTo implements CommandExecutor {
         return true;
     }
 
-    public void openBook(final Player player) {
+    public static void generateBook(){
+        howToBook = new ItemStack(Material.WRITTEN_BOOK);
+        BookMeta bookMeta = (BookMeta) howToBook.getItemMeta();
+
+        bookMeta.setAuthor("Paulsen__");
+        bookMeta.setTitle("How2 ExtendedPvP");
+        bookMeta.setLore(Arrays.asList("This is a HowTo-Book", "of the usefulness of the", "Extended-PvP plugin!"));
+        bookMeta.setPages(getPages());
+
+        howToBook.setItemMeta(bookMeta);
+    }
+
+    private static ArrayList<String> getPages(){
+        ArrayList<String> pages = new ArrayList<>();
+
+        // Mainpage
+        pages.add("    " + ChatColor.LIGHT_PURPLE + ChatColor.UNDERLINE + "<Extended PvP>\n\n" + ChatColor.RESET +
+                ChatColor.BLUE+ChatColor.BOLD+ChatColor.UNDERLINE+"Usage:"+(Extended_PvP.enabled ? (ChatColor.GREEN+" ENABLED") : (ChatColor.RED+" DISABLED"))+"\n\n"+
+                ChatColor.BLUE+ChatColor.BOLD+ChatColor.UNDERLINE + "Features:\n\n"+ChatColor.RESET+
+                ChatColor.BLACK+"- "+ChatColor.GOLD+"Collect heads"+ChatColor.RESET+" of other players by killing them.\n\n"+
+                "- "+ChatColor.GOLD+"Keep valuables"+ChatColor.RESET+" after getting killed by another player (e.g. armor, weapons)");
+
+        pages.add(ChatColor.RESET+"After being "+ChatColor.UNDERLINE+"killed by a Player"+ChatColor.RESET+", all items are dropped, except of a few other items which is set by the Admin.\n\n\n"+
+                ChatColor.BLUE+ChatColor.BOLD+ChatColor.UNDERLINE + "Settings:\n\n"+
+                ChatColor.RESET+"- You keep "+ChatColor.GOLD+ChatColor.UNDERLINE+(100d-Extended_PvP.killDropRate*100)+"%"+ChatColor.RESET+" of the "+ChatColor.UNDERLINE+"valuables.\n\n"+
+                ChatColor.RESET+"- Valuable-List:\n");
+
+        pages.addAll(printTable());
+
+        return pages;
+    }
+
+    private static List<String> printTable(){
+        ArrayList<String> s = new ArrayList<>();
+
+        StringBuilder b = new StringBuilder();
+        for(int i=0;i<Extended_PvP.dropTable.size();i++){
+            if(i % 14 == 0 && i != 0){
+                s.add(b.toString());
+                b.delete(0,b.length());
+            }
+            b.append(ChatColor.GOLD);
+            b.append(Extended_PvP.dropTable.get(i));
+            b.append("\n");
+        }
+        s.add(b.toString());
+
+        return s;
+    }
+
+    private static void openBook(final Player player) {
         player.openBook(howToBook);
     }
 }
